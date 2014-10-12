46bc3f6db5aab6a43dd853100227bc7ef0436dce
Winston Sumalia <wasumalia@gmail.com>
2014-10-13 01:38:43 +0800

    test commit

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,18 +1,22 @@
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   ##setwd("/Users/totomai/Documents/R Programming - Coursera/testfunctions")
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   c(directory,pollutant,id) 
-  1+1
-
+  ##paste(directory,sep)
+  dirlist<-list.files(path="specdata",pattern=".csv")
+  dirlist[1]  
+  
+  
   
   ## 'pollutant' is a character vector of length 1 indicating
   ## the name of the pollutant for which we will calculate the
   ## mean; either "sulfate" or "nitrate".
-
+  
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
-
+  
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
 }
