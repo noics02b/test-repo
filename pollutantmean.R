bb487db9b988e81119ccae1e9f699a32976301a5
Winston Sumalia <wasumalia@gmail.com>
2014-10-15 23:30:42 +0800

    next step: learn to combine data frames in a list in order to be able to implement one function like mean

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,13 +1,21 @@
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   ##setwd("/Users/totomai/Documents/R Programming - Coursera/testfunctions")
+  ##source("test-repo/pollutantmean.R")
 
   dirlist<-list.files(path="specdata",pattern=".csv",full.names=TRUE)  ##scans the path and creates a list of the filenames into dirlist
   
+  
   ##testing ongoing
   print (c("Monitoring these files:",dirlist[id]))  ## --test--print out monitor files read
-  lapply(dirlist[id],read.csv)  #stores the file data into memory
-
+  mem<-lapply(dirlist[id],read.csv)  #stores the file data into memory as data.frames
+  ## need to combine mem[id] to 1 matrix
+  ##print(str(mem[id]))
+  str(mem[id][[1]])
+  ##  test --mean(mem[id][[1]][[2]],na.rm=TRUE) # output list -- id no. -- sulfate or nitrate (2 or 3)
+  
+  
+  
   ##a<- pollutantmean("specdata","sulfate",1:3)
 
   
