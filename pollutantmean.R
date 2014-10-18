6c7d7e2748205d46e6ccb3d577dcc1e71d73c8ca
Winston Sumalia <wasumalia@gmail.com>
2014-10-19 00:40:47 +0800

    This code got 1/4 only :(

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,20 +1,19 @@
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ##setwd("/Users/totomai/Documents/R Programming - Coursera/testfunctions")
+  ##setwd("/Users/totomai/Documents/R Programming - Coursera/testfunctions/test-repo")
   ##source("test-repo/pollutantmean.R")
 
   dirlist<-list.files(path="specdata",pattern=".csv",full.names=TRUE)  ##scans the path and creates a list of the filenames into dirlist
   
   
   ##testing ongoing
-  print (c("Monitoring these files:",dirlist[id]))  ## --test--print out monitor files read
+  ##print (c("Monitoring these files:",dirlist[id]))  ## --test--print out monitor files read
   mem<-lapply(dirlist[id],read.csv)  #stores the file data into memory as data.frames
   ## need to combine mem[id] to 1 matrix
   ##print(str(mem[id]))
-  str(mem[id][[2]])
+  ##str(mem[id][[2]])
   ## mem[id][[3]]
   ##  test --mean(mem[id][[1]][[2]],na.rm=TRUE) # output list -- id no. -- sulfate or nitrate (2 or 3)
-  ##lapply(mem[id],mean, na.rm=TRUE)
   
   
   if (pollutant=="sulfate") {pick = 2}
@@ -23,28 +22,11 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ##a<-rbind(mem[[1]],mem[[2]])
   
   newdata<-NA
-  for (i in length(id)) {print (pollutant)
+  for (i in length(id)) {##print (pollutant)
                newdata<-rbind(newdata,mem[[i]])                }
   ##return (newdata)
   print(mean(newdata[[pick]],na.rm=TRUE))
   
-
-  
-  
   
   ##a<- pollutantmean("specdata","sulfate",1:3)
-
-  
-  
-  
-  ## 'pollutant' is a character vector of length 1 indicating
-
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-  
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
 }
