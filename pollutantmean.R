2f9bace055b50531ef6ecbe37cccd70a53ff340b
Winston Sumalia <wasumalia@gmail.com>
2014-10-19 00:02:35 +0800

    Finally, an attempt to compute for the mean

--- pollutantmean.R
+++ pollutantmean.R
@@ -11,8 +11,24 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   mem<-lapply(dirlist[id],read.csv)  #stores the file data into memory as data.frames
   ## need to combine mem[id] to 1 matrix
   ##print(str(mem[id]))
-  str(mem[id][[1]])
+  str(mem[id][[2]])
+  ## mem[id][[3]]
   ##  test --mean(mem[id][[1]][[2]],na.rm=TRUE) # output list -- id no. -- sulfate or nitrate (2 or 3)
+  ##lapply(mem[id],mean, na.rm=TRUE)
+  
+  
+  if (pollutant=="sulfate") {pick = 2}
+  if (pollutant=="nitrate")  {pick =3}
+  mem[id]
+  ##a<-rbind(mem[[1]],mem[[2]])
+  
+  newdata<-NA
+  for (i in length(id)) {print (pollutant)
+               newdata<-rbind(newdata,mem[[i]])                }
+  ##return (newdata)
+  print(mean(newdata[[pick]],na.rm=TRUE))
+  
+
   
   
   
