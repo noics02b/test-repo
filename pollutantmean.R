86c81f14e2e20a6cb5974756c8e3d2e3aaaa3373
Winston Sumalia <wasumalia@gmail.com>
2014-10-19 12:07:31 +0800

    Finalized pollutantmean.R code -- working and cleaned up with comments

--- pollutantmean.R
+++ pollutantmean.R
@@ -4,34 +4,17 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ##source("test-repo/pollutantmean.R")
 
   dirlist<-list.files(path="specdata",pattern=".csv",full.names=TRUE)  ##scans the path and creates a list of the filenames into dirlist
-  
-  
-  ##testing ongoing
-  ##print (c("Monitoring these files:",dirlist[id]))  ## --test--print out monitor files read
-  mem<-lapply(dirlist[id],read.csv)  #stores the file data into memory as data.frames
-  
-  ## need to combine mem[id] to 1 matrix
-  ##print(str(mem[id]))
-  ##str(mem[id][[2]])
-  ## mem[id][[3]]
+  mem<-lapply(dirlist[id],read.csv)  ##stores the file data into memory as data.frames
   ##  test --mean(mem[id][[1]][[2]],na.rm=TRUE) # output list -- id no. -- sulfate or nitrate (2 or 3)
   
-  
+  ## Given 2 fixed inputs corresponding to columns 2 and 3 on the mem (data frame)
   if (pollutant=="sulfate") {pick = 2}
   if (pollutant=="nitrate")  {pick =3}
-  mem[id]
-  ##a<-rbind(mem[[1]],mem[[2]])
   
-  newdata<-NA
-  for (i in 1:length(id)) { ##print (a<-paste("mem[",i,"]",collapse=""))}
-              newdata<-rbind(newdata,mem[[i]]) }
-  write.csv(newdata, "data.csv", row.names=FALSE)
+  newdata<-NA ## initial value, will be ignored anyway by the mean function
+  for (i in 1:length(id)) { 
+              newdata<-rbind(newdata,mem[[i]])  ## compiles all monitor data into just 1
+                          }
+  write.csv(newdata, "data.csv", row.names=FALSE)  ##bonus: writes the converged data frame into a file data.csv for checking
   return (mean(newdata[[pick]],na.rm=TRUE))
-              
-  
-  
-  ##print(mean(newdata[[pick]],na.rm=TRUE))
-  
-  
-  ##a<- pollutantmean("specdata","sulfate",1:3)
-}
+              }
